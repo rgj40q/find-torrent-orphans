eae9e9d87450d9362b86daaf428ff2c17dabe904
Margarito Tengben <45658038+rgj40q@users.noreply.github.com>
2025-03-06 21:12:43 +0700

    Delete free-ssd.sh

--- free-ssd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pwsh "C:\\Users\\$USER\\Desktop\\bin\\RT_Pack-main2\\Mover.ps1" -category '' -max_1_size '-1' -max_size '-1' -id_subfolder Y -path_from 'Z:\a\' -path_to 'N:\d\' -client_name client3
-
-pwsh "C:\\Users\\$USER\\Desktop\\bin\\RT_Pack-main\\Mover.ps1" -category '' -max_1_size '-1' -max_size '-1' -id_subfolder Y -path_from 'Z:\a\' -path_to 'N:\d\' -client_name client2
-
-cd "Z:\\"
-rmdir */*/* */* * 2>&-
